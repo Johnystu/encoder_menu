4cef6c622fce4ca0d0d352f80a309655ca96adf5
Elias <igolubevssmnu@mail.ru>
2017-07-04 16:10:23 +0300

    Style formate

--- C_M.ino
+++ C_M.ino
@@ -1,18 +1,18 @@
 void CM(byte n)  {
     if (r[n].STS.isRun == 1 ){
-     
-    startTime = millis();
-    stopTime  = startTime + (EEPROM.read(r[n].PAR.V) / EEPROM.read(r[n].PAR.K) * 1000);
-  }
-  if (r[n].STS.isRun && (stopTime < millis() )){
-    r[n].CS.CONTROL = 0;
-    r[n].STS.isRun = 0;
-  }
+      startTime = millis();
+      stopTime  = startTime + (EEPROM.read(r[n].PAR.V) / EEPROM.read(r[n].PAR.K) * 1000);
+    }
+    if (r[n].STS.isRun && (stopTime < millis() )){
+      r[n].CS.CONTROL = 0;
+      r[n].STS.isRun = 0;
+    }
   
-  if ( r[n].STS.isRun ){
-    digitalWrite(EEPROM.read(r[n].PAR.Pin), HIGH);
-    r[n].PAR.PV = (stopTime - millis())/1000;
-  }
-    else {digitalWrite(EEPROM.read(r[n].PAR.Pin), LOW);}      
-  }
-    
+    if ( r[n].STS.isRun ){
+      digitalWrite(EEPROM.read(r[n].PAR.Pin), HIGH);
+      r[n].PAR.PV = (stopTime - millis())/1000;
+    }
+    else {
+        digitalWrite(EEPROM.read(r[n].PAR.Pin), LOW);
+    }      
+} 
